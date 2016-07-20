bbb90eff57829cd6f537a1cd6f4e1d04f62b7aa8
Andrew Deck <deck.andrew@gmail.com>
2016-07-20 12:13:25 -0400

    Initial Commit

---
+++ Teatime/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Teatime
+//
+//  Created by andrew deck on 7/20/16.
+//  Copyright © 2016 Andrew Deck. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
