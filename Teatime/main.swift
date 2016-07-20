1c0bdd8560e86d4992b47a6f340ef073ce8b6e92
Andrew Deck <deck.andrew@gmail.com>
2016-07-20 13:28:04 -0400

    New version of Teatime with swift for OS X.

--- Teatime/main.swift
+++ Teatime/main.swift
@@ -7,6 +7,42 @@
 //
 
 import Foundation
+import AppKit
+import CoreGraphics
 
-print("Hello, World!")
+func flashScreen(times:Int) {
+    let duration: CGDisplayFadeInterval = 0.75
+    
+    var fadeToken: CGDisplayFadeReservationToken = 0
+    let err = CGAcquireDisplayFadeReservation(15, &fadeToken)
+    if Int(err.rawValue) != Int(0) {
+        NSLog("Error acquiring fade reservation")
+        return
+    }
+    
+    for _ in 1...times {
+        CGDisplayFade(fadeToken, duration/2,
+                      0.0 as CGDisplayBlendFraction, 0.8 as CGDisplayBlendFraction,
+                      0.0, 0.0, 0.0,
+                      boolean_t(1))
+        CGDisplayFade(fadeToken, duration/2,
+                      0.8 as CGDisplayBlendFraction, 0.0 as CGDisplayBlendFraction,
+                      0.0, 0.0, 0.0,
+                      boolean_t(1))
+    }
+    CGReleaseDisplayFadeReservation(fadeToken)
+}
+
+
+
+if Process.arguments.count < 2 {
+    print("Usage: teatime <number of minutes to steep>")
+    exit(EXIT_FAILURE)
+} else {
+    let steepTime = Process.arguments[1]
+    let intSteepTime = UInt32(steepTime)!
+    
+    sleep(intSteepTime*60)
+    flashScreen(3)
+}
 
